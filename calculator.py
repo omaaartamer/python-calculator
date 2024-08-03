e5da3b299397bf0a50bd7bcf111db1d480faa01b
Omar Tamer <omar.tamer@orange.com>
2024-08-03 16:14:27 +0300

    fixed linting issues

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
-import sys
-
 """
 This module is a basic calculator that performs basic operations on 2 numbers.
 """
+import sys
+
 
 def add(a, b):
     """
@@ -17,9 +17,11 @@ def add(a, b):
     """
     return a + b
 
+
 def subtract(c, d):
     """Return the difference of c and d."""
     return c - d
+
 
 def multiply(e, f):
     """
@@ -34,17 +36,27 @@ def multiply(e, f):
     """
     return e * f
 
+
 def divide(g, h):
     """Return the division of g by h. Raise ValueError if h is zero."""
     if h == 0:
         raise ValueError("Cannot divide by zero")
     return g / h
 
+
 def modulus(i, j):
     """Return the modulus of i by j."""
     return i % j
 
-def main(x,y, oper):
+
+def main(x, y, oper):
+    """_summary_
+
+    Args:
+        x (float): first number
+        y (float): second number
+        oper (string): operation to be performed
+    """
     # Ensure that the correct number of arguments are provided
     if len(sys.argv) != 4:
         print("Usage: python calculator.py <number1> <number2> <operation>")
@@ -81,7 +93,8 @@ def main(x,y, oper):
         except ValueError as e:
             print(f"Error: {e}")
     else:
-        print("Unsupported Operation. Supported operations: add, subtract, multiply, divide, modulus.")
+        print("Unsupported Operation.")
+
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2],sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
